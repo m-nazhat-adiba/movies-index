7ac816d5074eb43245317338a501b08c7f7a2ade
Muhammad Nazhat Adiba <m.nazhat.adiba@gmail.com>
2023-08-05 08:28:31 +0700

    added next & previous button function

--- pagination.js
+++ pagination.js
@@ -1,10 +1,46 @@
 const paginationNumbers = document.getElementById("pagination-numbers");
 const paginatedList = document.getElementById("paginated-list");
 const listItems = paginatedList.querySelectorAll("a");
+const nextButton = document.getElementById("next-button");
+const prevButton = document.getElementById("prev-button");
 
 const paginationLimit = 12;
 const pageCount = Math.ceil(listItems.length / paginationLimit);
 let currentPage = 1;
+
+const disableButton = (button) => {
+  button.classList.add("disabled");
+  button.setAttribute("disabled", true);
+};
+
+const enableButton = (button) => {
+  button.classList.remove("disabled");
+  button.removeAttribute("disabled");
+};
+
+const handlePageButtonsStatus = () => {
+  if (currentPage === 1) {
+    disableButton(prevButton);
+  } else {
+    enableButton(prevButton);
+  }
+
+  if (pageCount === currentPage) {
+    disableButton(nextButton);
+  } else {
+    enableButton(nextButton);
+  }
+};
+
+const handleActivePageNumber = () => {
+  document.querySelectorAll(".pagination-number").forEach((button) => {
+    button.classList.remove("active");
+    const pageIndex = Number(button.getAttribute("page-index"));
+    if (pageIndex == currentPage) {
+      button.classList.add("active");
+    }
+  });
+};
 
 const appendPageNumber = (index) => {
   const pageNumber = document.createElement("button");
@@ -22,19 +58,11 @@ const getPaginationNumbers = () => {
   }
 };
 
-const handleActivePageNumber = () => {
-  document.querySelectorAll(".pagination-number").forEach((button) => {
-    button.classList.remove("active");
-    const pageIndex = Number(button.getAttribute("page-index"));
-    if (pageIndex == currentPage) {
-      button.classList.add("active");
-    }
-  });
-};
-
 const setCurrentPage = (pageNum) => {
   currentPage = pageNum;
+
   handleActivePageNumber();
+  handlePageButtonsStatus();
 
   const prevRange = (pageNum - 1) * paginationLimit;
   const currRange = pageNum * paginationLimit;
@@ -50,6 +78,14 @@ const setCurrentPage = (pageNum) => {
 window.addEventListener("load", () => {
   getPaginationNumbers();
   setCurrentPage(1);
+
+  prevButton.addEventListener("click", () => {
+    setCurrentPage(currentPage - 1);
+  });
+
+  nextButton.addEventListener("click", () => {
+    setCurrentPage(currentPage + 1);
+  });
 
   document.querySelectorAll(".pagination-number").forEach((button) => {
     const pageIndex = Number(button.getAttribute("page-index"));
