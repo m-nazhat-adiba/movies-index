e165c3d39ed853df67d930a864dc5ac1a33c6c03
Muhammad Nazhat Adiba <m.nazhat.adiba@gmail.com>
2023-08-08 21:51:46 +0700

    added javascript for card carousel 2

---
+++ swiperCarousel2.js
@@ -0,0 +1,38 @@
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 6,
+  spaceBetween: 10,
+  centeredSlides: false,
+  //   slidesPerGroupSkip: 1,
+  grabCursor: true,
+  keyboard: {
+    enabled: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      slidesPerGroup: 2,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+      slidesPerGroup: 4,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 12,
+      slidesPerGroup: 6,
+    },
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  navigation: {
+    nextEl: "#swiper-button-next-2",
+    prevEl: "#swiper-button-prev-2",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
