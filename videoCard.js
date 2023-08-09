a634abc5a2c9c5c609770b7614761d305d08b9df
Muhammad Nazhat Adiba <m.nazhat.adiba@gmail.com>
2023-08-09 15:37:43 +0700

    edited video card layout

--- videoCard.js
+++ videoCard.js
@@ -15,6 +15,7 @@ href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.c
 class="card w-100 border-0"
 style="background-color: transparent"
 >
+<div class="position-relative">
 <img
   class="card-img-top"
 />
@@ -22,15 +23,16 @@ style="background-color: transparent"
   class="card-img-overlay d-flex flex-column text-white justify-content-center"
 >
   <div
-    class="d-flex flex-row align-items-center justify-content-center pb-3"
+    class="d-flex flex-row align-items-center justify-content-center "
   >
     <i class="bi bi-play-circle" style="font-size: 2rem"></i>
-    <p class="card-text fs-5 ps-2">2:26</p>
+    <p class="card-text fs-5 ps-2 len"></p>
   </div>
+</div>
 </div>
 <a class="card-img-overlay"></a>
 <h4 class="card-title fs-5 text-white"></h4>
-<p class="card-text fs-6 text-white"></p>
+<p class="card-text fs-6 text-white date"></p>
 </div>
 `;
 
@@ -44,8 +46,8 @@ class videoCard extends HTMLElement {
       this.getAttribute("movieTitle");
     this.shadowRoot.querySelector(".date").innerText =
       this.getAttribute("date");
-    this.shadowRoot.querySelector(".len").href =
-      this.getAttribute("targetLink");
+    this.shadowRoot.querySelector(".len").innerText = this.getAttribute("len");
+    this.shadowRoot.querySelector("a").href = this.getAttribute("targetLink");
   }
 
   connectedCallback() {
