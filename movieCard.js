14bd96e86123a84b9e59aa79323f97a783e32a24
Muhammad Nazhat Adiba <m.nazhat.adiba@gmail.com>
2023-08-12 09:59:42 +0700

    added button to template and changed the shadow DOM to DOM

--- movieCard.js
+++ movieCard.js
@@ -1,11 +1,5 @@
 const template = document.createElement("template");
 template.innerHTML = `
-<link
-href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
-rel="stylesheet"
-integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
-crossorigin="anonymous"
-/>
 <style>
 .cardGradient {
   background: rgb(0, 0, 0);
@@ -26,18 +20,19 @@ crossorigin="anonymous"
   <p class="card-text fs-6"></p>
 </div>
 <a class="card-img-overlay" href="#"></a>
+<button type="button" class="btn btn-link p-0" data-bs-toggle="modal" data-bs-target="#notificationModal">
+<i class="position-absolute bi bi-bookmark-plus-fill" style="font-size: 32px; left: -2px; top: -10px"></i>
+</button>
 </div>`;
 
 class movieCard extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-    this.shadowRoot.querySelector("img").src = this.getAttribute("poster");
-    this.shadowRoot.querySelector("h4").innerText =
-      this.getAttribute("movieTitle");
-    this.shadowRoot.querySelector("p").innerText = this.getAttribute("year");
-    this.shadowRoot.querySelector("a").href = this.getAttribute("targetLink");
+    this.appendChild(template.content.cloneNode(true));
+    this.querySelector("img").src = this.getAttribute("poster");
+    this.querySelector("h4").innerText = this.getAttribute("movieTitle");
+    this.querySelector("p").innerText = this.getAttribute("year");
+    this.querySelector("a").href = this.getAttribute("targetLink");
   }
 
   connectedCallback() {
